2986f8d1a496de37ea380d2dfbf7e9587c8bd338
jf-garamendi <jf.garamendi@upf.edu>
2021-05-21 12:20:09 +0200

    Still not working code

--- agents/encDec_update_A.py
+++ agents/encDec_update_A.py
@@ -1,8 +1,13 @@
 """
 The Base Agent class, where all other agents inherit from, that contains definitions for all the necessary functions
 """
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..')))
 import logging
 from agents.base import  BaseAgent
+from os.path import join
+from training.VideoInp_DataSet import VideoInp_DataSet
 
 class EncDec_update_agent_A(BaseAgent):
     """
@@ -12,8 +17,22 @@ class EncDec_update_agent_A(BaseAgent):
     def __init__(self,  config, **models):
         super().__init__(config)
 
-        self.config = config
-        self.logger = logging.getLogger("Agent")
+        # Folders
+        self.exp_name = config.general.exp_name
+        self.TB_stats_dir = join(config.verbose.tensor_board_root_dir, self.exp_name)
+        self.checkpoint_dir = join(config.checkpoint.root_dir, self.exp_name)
+
+        #Datasets
+        encDec_train_data = VideoInp_Dataset(config.data.train_root_dir,
+                                             GT = True,
+                                             number_of_frames = config.data.number_of_frames,
+           AQUI ME HE QUEDADO
+
+        )
+
+
+
+
 
     def load_checkpoint(self, file_name):
         """
