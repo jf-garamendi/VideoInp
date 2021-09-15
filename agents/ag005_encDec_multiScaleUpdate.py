aee6faca14276246bbd919ecac04650770da0717
jf-garamendi <jf.garamendi@upf.edu>
2021-09-15 13:18:40 +0200

    duplicate the optical flow length when change the scale (from coarser to finer)

--- agents/ag005_encDec_multiScaleUpdate.py
+++ agents/ag005_encDec_multiScaleUpdate.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import torchvision.transforms as T
 
 from ingestion import *
-from utils.data_io import  verbose_images
+from utils.data_io import verbose_images, tensor_save_flow_and_img, save_mask_tensor_as_img
 import numpy as np
 
 class Ag005_EncDec_MultiScaleUpdate(EncDec_update_agent_001):
@@ -32,29 +32,52 @@ class Ag005_EncDec_MultiScaleUpdate(EncDec_update_agent_001):
             previous_scale_confidence = 0
             previous_scale_flow = 0
             #DEBUG
+            # choose (harcoded) the number of levels in the pyramid_data
             pyramid_data = [ pyramid_data[i][-2:] for i in range(len(pyramid_data))]
             # end DEBUG
-            for iflows, masks, gt_flows in zip(pyramid_data[1], pyramid_data[2], pyramid_data[4]):
+            for iflows_o, masks, gt_frames, gt_flows in zip(pyramid_data[1], pyramid_data[2], pyramid_data[3], pyramid_data[4]):
                 # from coarsest to finest
-                B, N, C, H, W = iflows.shape
+                B, N, C, H, W = iflows_o.shape
 
                 # Remove the batch dimension (for pierrick architecture is needed B to be 1)
 
                 # masks: 1 inside the hole
                 masks = masks.view(B * N, 1, H, W)
+                gt_frames = gt_frames.view(B * N, 3, H, W)
 
                 gt_flows = gt_flows.view(B * N, C, H, W)
+                iflows_o = iflows_o.view(B * N, C, H, W)
 
                 # place data on device
-                iflows = iflows.to(self.device)
+                iflows_o = iflows_o.to(self.device)
                 masks = masks.to(self.device)
                 gt_flows = gt_flows.to(self.device)
+                gt_frames = gt_frames.to(self.device)
 
                 # Initial confidence: 1 outside the mask (the hole), 0 inside
-                initial_confidence = (1 - 1. * masks) + previous_scale_confidence * 0.25
+                initial_confidence = (1 - 1. * masks) + previous_scale_confidence * 0.5
                 initial_confidence = torch.clip(initial_confidence, 0, 1)
 
-                iflows = (1-1. * masks)*iflows.view(B * N, C, H, W) + previous_scale_flow * (masks)
+                uno = torch.Tensor([1]).to(self.device)
+                iflows_a = (1-masks.type(torch.FloatTensor)).to(self.device)*iflows_o
+                iflows_b = 2*previous_scale_flow * (masks.type(torch.FloatTensor)).to(self.device)
+                iflows = iflows_a + iflows_b
+
+                '''
+                tensor_save_flow_and_img(iflows_a[:, 0:2, :, :], "./borrar/input_flow_a")
+                if previous_scale_flow is not 0:
+                    tensor_save_flow_and_img(iflows_b[:, 0:2, :, :], "./borrar/input_flow_b")
+                    save_mask_tensor_as_img(previous_scale_confidence, "./borrar/previous_scale_confidence")
+                    tensor_save_flow_and_img(computed_flows[:, 0:2, :, :], "./borrar/grueso")
+                    tensor_save_flow_and_img(previous_scale_flow[:, 0:2, :, :], "./borrar/fino")
+
+                tensor_save_flow_and_img(iflows[:, 0:2, :, :], "./borrar/input_flow")
+                tensor_save_flow_and_img(iflows_o[:, 0:2, :, :], "./borrar/input_flow_orig")
+                save_mask_tensor_as_img(masks, "./borrar/masks")
+                save_mask_tensor_as_img(initial_confidence, "./borrar/initial_confidence")
+                '''
+
+
                 confidence = initial_confidence.clone()
                 gained_confidence = initial_confidence
 
@@ -109,10 +132,11 @@ class Ag005_EncDec_MultiScaleUpdate(EncDec_update_agent_001):
                 previous_scale_flow = T.Resize(size=(H*2, W*2))(computed_flows)
 
 
+
             if verbose:
                 verbose_images(self.verbose_out_images, prefix='update_sec_$'+sec_name[0]+'$',
                                input_flow=iflows, computed_flow=computed_flows,
-                               gt_flow=gt_flows)
+                               gt_flow=gt_flows, frames=gt_frames , masks=masks)
             nSec += 1
 
         return loss2print
